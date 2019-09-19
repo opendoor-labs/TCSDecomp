0d66f2509bf3a033a52f3c4f4ea4e84043a05a54
alexhubbardOD <alex.hubbard@opendoor.com>
2019-09-19 14:32:51 -0700

    test

--- R/package_functions.R
+++ R/package_functions.R
@@ -230,7 +230,7 @@ tcs_decomp_estim = function (y, freq = NULL, decomp = NULL, trend_spec = NULL, d
     #Calculate a periodogram for the data
     pgram = TSA::periodogram(imputeTS::na_kalman(y), plot = F)
     pgram = data.table::data.table(freq = pgram$freq, spec = pgram$spec, period = 1/pgram$freq)[order(-spec), ]
-    print(pgram)
+    print(class(pgram))
     pgram[, `:=`("d", (spec)/mean(spec, na.rm = T))]
     pgram = pgram[period < length(y), ]
 
